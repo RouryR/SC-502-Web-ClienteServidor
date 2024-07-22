50d7ab02c20ca1e468140b4b27316ddf08552be5
MarcoDGCH19 <marcochaves0919@gmail.com>
2024-07-22 17:11:59 -0600

    include de php
    Co-Authored-By: Cerem0208 <128267789+Cerem0208@users.noreply.github.com>
    Co-Authored-By: RAMIREZ CORRALES DIEGO JOSE <92670091+RouryR@users.noreply.github.com>

--- static/routes/productos/nominaYRecursos.php
+++ static/routes/productos/nominaYRecursos.php
@@ -8,12 +8,7 @@ ini_set('display_errors', 1);
 
 <head>
     <title>Nómina y Recursos Humanos</title>
-    <link rel="stylesheet" href="../../../node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <script src="../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://kit.fontawesome.com/3b9cff258c.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="/SC-502-Web-ClienteServidor/static/css/style.css?n=<?php echo(rand()); ?>">
-    <link rel="stylesheet" href="/SC-502-Web-ClienteServidor/static/css/routes_style.css?n=<?php echo(rand()); ?>">
-    <script src="./SC-502-Web-ClienteServidor/static/js/script.js"></script>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/SC-502-Web-ClienteServidor/static/php/head.php'; ?>
 </head>
 
 <body>
